9afba251e3e3e46bf67a48583eb79dbeffd95f53
Ahasan Habib <habibsumoncse@gmail.com>
2020-11-08 10:51:01 +0600

    add security in resource service

--- micro-product-service/src/main/java/com/product/rest/ProductApplicationRunner.java
+++ micro-product-service/src/main/java/com/product/rest/ProductApplicationRunner.java
@@ -3,6 +3,8 @@ package com.product.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
 
 
 @SpringBootApplication
