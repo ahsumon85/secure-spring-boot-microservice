0f4b7c82e70ded9504044cf8139dfc797ba4f68a
Ahasan Habib <habibsumoncse@gmail.com>
2020-12-16 10:30:35 +0600

    change auth configure

--- micro-auth-service/src/main/java/com/ahasan/auth/AuthorizationRunner.java
+++ micro-auth-service/src/main/java/com/ahasan/auth/AuthorizationRunner.java
@@ -3,10 +3,8 @@ package com.ahasan.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
-@EnableAuthorizationServer
 @EnableEurekaClient
 public class AuthorizationRunner {
 
--- micro-auth-service/src/main/java/com/ahasan/auth/config/AuthorizationServerConfiguration.java
+++ micro-auth-service/src/main/java/com/ahasan/auth/config/AuthorizationServerConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -14,6 +15,7 @@ import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
 
+@EnableAuthorizationServer
 @Configuration
 public class AuthorizationServerConfiguration implements AuthorizationServerConfigurer {
 
